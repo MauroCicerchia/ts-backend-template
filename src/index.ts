80db504e9bf5477924d8591690ea74c8184f312d
Mauro Cicerchia <maurocicerchia98@gmail.com>
2022-04-03 16:59:45 -0300

    eslint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express, { Application, Request, Response } from 'express';
-import bodyParser from 'body-parser';
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
 import * as dotenv from "dotenv";
-import cors from 'cors';
+import cors from "cors";
 
 dotenv.config();
 
@@ -9,14 +9,14 @@ const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: true }))
+app.use(cors({ origin: true }));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Healthy')
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Healthy");
+});
 
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT ${PORT}`)
-})
+  console.log(`Server is running on PORT ${PORT}`);
+});
