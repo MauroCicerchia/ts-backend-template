823a0c8b9485e0058824a40314af97e7785d6750
Mauro Cicerchia <maurocicerchia98@gmail.com>
2022-04-03 16:02:27 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import * as dotenv from "dotenv";
+import cors from 'cors';
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app: Application = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: true }))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Healthy')
